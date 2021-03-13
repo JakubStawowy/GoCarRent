4bea4173706bdd3c61a1087e0cde761da2349f07
Jakub <stawowykuba@gmail.com>
2021-03-13 21:20:56 +0100

    initial commit

---
+++ go-car-rent-spring-boot-application/src/main/java/com/example/gocarrentspringbootapplication/GoCarRentSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.gocarrentspringbootapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoCarRentSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoCarRentSpringBootApplication.class, args);
+    }
+
+}
